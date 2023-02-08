5613ecf0b45154d2db5eb796920b066ceb6cc5b8
Crowdbotics <team@crowdbotics.com>
2023-02-08 16:59:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/online_money_apk_38816/MainActivity.java
@@ -0,0 +1,15 @@
+package com.online_money_apk_38816;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "online_money_apk_38816";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "online-money-apk-38816",
+  url: "https://online-money-apk-38816.botics.co",
+  api: "https://online-money-apk-38816.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
